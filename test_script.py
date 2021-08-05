50b3850fa62da3362a3a03e59b0db726f256708b
Christos Athanasiadis <xathanas@gmail.com>
2021-08-05 19:24:25 +0300

    Test script added.

--- test_script.py
+++ test_script.py
@@ -1,20 +1,19 @@
+# demo script
 from smart_grid import SmartGrid
 
+# Initialize the grid. In this case, there are no PV and batteries. By editing the corresponding cells
+# of the csv file, you can add PV and battery to whichever home you prefer.
+grid = SmartGrid('test_cases/test_case_1_minor_over_voltage/grid_IEEE.csv', name='IEEE')
 
-# grid = SmartGrid('test_grids/grid_JRC.csv')
-# grid = SmartGrid('test_grids/grid_IEEE.csv', name='IEEE')
-grid = SmartGrid('test_cases/grid_IEEE.csv', name='IEEE')
-
-simulation_file = 'test_grids/simulation_parameters.csv'
+# Set the simulation parameters and calculate the power for each home.
+simulation_file = 'test_cases/test_case_1_minor_over_voltage/simulation_parameters.csv'
 grid.set_simulation_parameters(simulation_file)
 grid.set_load_consumption()
 
+# Add solar panels at free nodes of the grid.
+grid.set_generators('test_cases/test_case_1_minor_over_voltage/generators.csv')
 
-# # grid.set_generators('test_grids/generators_JRC.csv')
-# grid.set_generators('test_grids/generators_IEEE.csv')
-
-
-# grid.solve_power_flow(dss_path='test_grids/IEEE.dss', number_of_seconds=30,
-#                       save_results=True, folder='13_6_2021')
-# #
+# Solve power flow for 30 seconds (starting at 1000th second) and save the results to the folder 'demo'
+grid.solve_power_flow(dss_path='test_cases/test_case_1_minor_over_voltage/IEEE.dss', number_of_seconds=30,
+                      starting_second=1000, save_results=True, folder='demo')
 
