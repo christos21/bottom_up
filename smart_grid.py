e7b8d6e327a518c2a6cadf6dcda2d7a4ad24b285
Christos Athanasiadis <xathanas@gmail.com>
2021-09-19 19:00:20 +0300

    Reading SmartHome load from mat file, power flow solver for passive network and more indexes added for SmartGrid.

--- smart_grid.py
+++ smart_grid.py
@@ -4,11 +4,14 @@ import sys
 import matplotlib.pyplot as plt
 import json
 import os
+from scipy.io import loadmat
 
 from smart_home import SmartHome
 from utils import check_for_consecutive_days, pv_profile_generator, PHASES
-from power_flow import solve_power_flow
-from indexes import substation_reserve_capacity, feeder_loss_to_load_ratio
+from power_flow import solve_power_flow, calculate_passive_network_losses_from_power_flow
+from indexes import substation_reserve_capacity, feeder_loss_to_load_ratio, voltage_unbalance_factors, \
+    losses_to_load_ratio, losses_reduction_index, mean_voltage_variance, voltage_level_quantification_index, \
+    average_feeding_loading_index
 
 
 class SmartGrid:
@@ -124,12 +127,69 @@ class SmartGrid:
             self.P_load += self[i].P
             self.Q += self[i].Q
 
-    def reset_pv_and_battery(self, pv_and_battery_df):
+    def set_load_consumption_from_mat_file(self, mat_file):
+        """
+        This method creates the load profile for each home which are saved in a .mat file.
+        :return:
+        """
+        data = loadmat(mat_file)
+
+        p = data['Pload']
+        q = data['Qload']
+
+        phases = data['phase_con']
+
+        assert len(self.days) == p.shape[0]//(60*60*24), 'Number of days in mat file and simulation parameters ' \
+                                                         'are different'
+
+        pv = data['pv']
+
+        homes = data['PQLoadname']
+
+        column = 0
+
+        for k, home in enumerate(homes):
+            home = home.strip()
+            assert self.homes[home]
+            if self.homes[home].single_phase:
+                phase = self.homes[home].selected_phase
+                assert phase == phases[k].strip(), 'Different phases in .mat and grid csv file for home {}'.format(home)
+                home_p = p[:, column]
+                home_q = q[:, column]
+
+                column += 1
+
+                self.homes[home].set_load_from_arrays(p_array=home_p, q_array=home_q, phase=phase, days=self.days)
+
+            else:
+
+                assert phases[k].strip() == 'abc', 'Different phases in .mat and grid csv file for home {}'.format(home)
+                home_p = p[:, column:column+3]
+                home_q = q[:, column:column+3]
+
+                column += 3
+
+                self.homes[home].set_load_from_arrays(p_array=home_p, q_array=home_q, days=self.days)
+
+            self.homes[home].set_pv_and_battery(days=self.days, month=self.month, pv_from_array=True, pv_array=pv)
+
+        # Sum aggregated P, P_load and Q (per phase) for all homes.
+        self.P = self[0].grid_power.copy()
+        self.P_load = self[0].P.copy()
+        self.Q = self[0].Q.copy()
+        for i in range(1, len(self.homes)):
+            self.P += self[i].grid_power
+            self.P_load += self[i].P
+            self.Q += self[i].Q
+
+    def reset_pv_and_battery(self, pv_and_battery_df, pv_from_array=False, pv_array=None):
         """
         Method that resets PV and battery properties for all homes.
         The time-series for PV production and battery charging/discharging are also reset.
         A new dataframe is needed including the properties of the new PV and batteries.
         :param pv_and_battery_df: pd.DataFrame
+        :param pv_from_array: bool
+        :param pv_array: np.array
         :return:
         """
 
@@ -141,7 +201,7 @@ class SmartGrid:
                 continue
 
             self.homes[home].reset_pv_and_battery_values(pv_and_battery_df[home])
-            self.homes[home].set_pv_and_battery(self.days, self.month)
+            self.homes[home].set_pv_and_battery(self.days, self.month, pv_from_array, pv_array)
 
         # Sum aggregated P (per phase) for all homes. P_load and Q remain the same since the new PV and
         # batteries do not affect the load or the reactive power.
@@ -150,13 +210,15 @@ class SmartGrid:
         for i in range(1, len(self.homes)):
             self.P += self[i].grid_power
 
-    def set_generators(self, pv_df=None, csv_file=None):
+    def set_generators(self, pv_df=None, csv_file=None, pv_from_array=False, pv_array=None):
         """
         Method for setting solar panels at free nodes of the grid.
         We can either pass the path to the csv file at parameter csv_file OR
         the pandas dataframe at parameter pv_df.
         :param pv_df: pd.DataFrame | None
         :param csv_file: str | None
+        :param pv_from_array: bool
+        :param pv_array: np.array
         :return:
         """
         self.generators = {}
@@ -171,34 +233,50 @@ class SmartGrid:
 
         for pv in df.columns:
             self.generators[pv] = PV(pv, df[pv])
-            self.generators[pv].set_power(self.days, self.month)
+            self.generators[pv].set_power(self.days, self.month, from_array=pv_from_array, pv_array=pv_array)
 
         for generator in self.generators.values():
             self.P += generator.P
 
-    def solve_power_flow(self, dss_path, number_of_seconds=None, starting_second=0, save_results=True, folder=None):
+    def solve_power_flow(self, dss_path, folder, number_of_seconds=None, starting_second=0, subsampling=1):
         """
         Method for solving power flow for the grid.
         :param dss_path: str, Path to a .dss file. The name of each home should be the name
                               of the bus that is connected to.
         :param number_of_seconds: int, Number of seconds to solve the power flow
         :param starting_second: int, The first second for which the power flow will be solved.
-        :param save_results: bool, Set to True to save results.
-        :param folder: str, Path of the folder where the results will be saved if save_results is True.
+        :param folder: str, Path of the folder where the results will be saved.
+        :param subsampling: int
         :return:
         """
 
-        if save_results:
-            path_to_grid_params, path_to_results = solve_power_flow(self, dss_path=dss_path,
-                                                                    number_of_seconds=number_of_seconds,
-                                                                    starting_second=starting_second,
-                                                                    save_results=save_results,
-                                                                    folder=folder)
-        else:
-            path_to_grid_params, path_to_results = None, None
+        solve_power_flow(self, dss_path=dss_path,
+                         number_of_seconds=number_of_seconds,
+                         starting_second=starting_second,
+                         save_results=True,
+                         folder=folder,
+                         subsampling=subsampling)
 
-        self.path_to_grid_params = path_to_grid_params
-        self.path_to_results = path_to_results
+        self.path_to_results = folder
+
+    def solve_passive_power_flow(self, dss_path, folder, number_of_seconds=None, starting_second=0,
+                                 subsampling=1):
+        """
+        Method for solving power flow for the grid assuming no PV and batteries.
+        :param dss_path: str, Path to a .dss file. The name of each home should be the name
+                              of the bus that is connected to.
+        :param number_of_seconds: int, Number of seconds to solve the power flow
+        :param starting_second: int, The first second for which the power flow will be solved.
+        :param folder: str, Path of the folder where the results will be saved if save_results is True.
+        :param subsampling: int
+        :return:
+        """
+        calculate_passive_network_losses_from_power_flow(self, dss_path=dss_path,
+                                                         number_of_seconds=number_of_seconds,
+                                                         starting_second=starting_second,
+                                                         save_results=True,
+                                                         folder=folder,
+                                                         subsampling=subsampling)
 
     def get_statistics(self):
         """
@@ -262,30 +340,17 @@ class SmartGrid:
 
         return homes_with_battery
 
-    def calculate_grid_indexes(self, path_to_grid_params=None, path_to_results=None):
+    def calculate_grid_indexes(self, path_to_grid_params, path_to_results, line_limits_file):
         """
         Calculates some grid-related indexes for PV-BESS assessment.
         For the calculation of these indexes, power flow should be solved and the results should be stored.
         The input parameters are the path of the result folder and the path of the json file
-        created by the solve_power_flow method. If solve_power_flow() is called in the same session, there is no
-        need to set the input parameters.
+        created by the solve_power_flow method.
 
-        :param path_to_grid_params: str | None
-        :param path_to_results: str | None
+        :param path_to_grid_params: str
+        :param path_to_results: str
         :return:
         """
-
-        if path_to_results is None:
-            path_to_results = self.path_to_results
-        if path_to_grid_params is None:
-            path_to_grid_params = self.path_to_grid_params
-
-        if path_to_grid_params is None:
-            print("Give path to grid parameters json file.")
-            return
-        if path_to_results is None:
-            print("Give path to results folder.")
-            return
 
         assert os.path.exists(path_to_grid_params)
         assert os.path.exists(path_to_results)
@@ -298,23 +363,56 @@ class SmartGrid:
         initial_bus = params['initial_bus']
         bus_names = params['bus_names']
         bus_connections = params['bus_connections']
+        r_to_bus = params['resistance_from_start_to_each_bus']
+        length_to_bus = params['length_from_start_to_each_bus']
+        line_length = params['line_length']
 
         load_names = list(self.homes.keys())
+
+        losses = pd.read_csv(os.path.join(path_to_results, 'Losses.csv'), index_col=0)
+        line_power = pd.read_csv(os.path.join(path_to_results, 'line_power.csv'), index_col=0)
+        Va = pd.read_csv(os.path.join(path_to_results, 'Va.csv'), index_col=0)
+        Vb = pd.read_csv(os.path.join(path_to_results, 'Vb.csv'), index_col=0)
+        Vc = pd.read_csv(os.path.join(path_to_results, 'Vc.csv'), index_col=0)
+        transformer_power = pd.read_csv(os.path.join(path_to_results, 'transformer_power.csv'), index_col=0)
+
+        passive_losses = pd.read_csv(os.path.join(path_to_results, 'Losses_for_passive_network.csv'), index_col=0)
+        passive_va = pd.read_csv(os.path.join(path_to_results, 'Va_for_passive_network.csv'), index_col=0)
+        passive_vb = pd.read_csv(os.path.join(path_to_results, 'Vb_for_passive_network.csv'), index_col=0)
+        passive_vc = pd.read_csv(os.path.join(path_to_results, 'Vc_for_passive_network.csv'), index_col=0)
 
         # calculate power balance index
         self.indexes['pbi'] = self.power_balance_index(bus_names, load_names, bus_connections, initial_bus)
 
         # calculate feeder loss to load ratio
         total_load = self.P_load.sum(axis=1)
-        losses = pd.read_csv(os.path.join(path_to_results, 'Losses.csv'), index_col=0)
-        total_load = total_load.iloc[:len(losses)]
+        total_load = total_load.loc[losses.index]  #total_load.iloc[:len(losses)]
         self.indexes['fllr'] = feeder_loss_to_load_ratio(total_load, losses.P)
 
         # calculate substation reserve capacity
-        transformer_power = pd.read_csv(os.path.join(path_to_results, 'transformer_power.csv'), index_col=0)
-        # s = transformer_power['P_s'] + transformer_power['Q_s']
         s = (transformer_power['P_s']**2 + transformer_power['Q_s']**2)**(1/2)
         self.indexes['src'] = substation_reserve_capacity(s, S_substation)
+
+        # calculate VUF0, VUF2
+        self.indexes['VUF0'], self.indexes['VUF2'] = voltage_unbalance_factors(Va, Vb, Vc)
+
+        # calculate lri and llr
+        self.indexes['llr'] = losses_to_load_ratio(total_load, losses.P)
+        self.indexes['lri'] = losses_reduction_index(losses.P, passive_losses.P)
+
+        # sigma
+        self.indexes['mean_voltage_variance_va'] = mean_voltage_variance(Va, '1s')
+        self.indexes['mean_voltage_variance_vb'] = mean_voltage_variance(Vb, '1s')
+        self.indexes['mean_voltage_variance_vc'] = mean_voltage_variance(Vc, '1s')
+
+
+        # vlqi
+        self.indexes['vlqi'] = voltage_level_quantification_index(Va, Vb, Vc, passive_va, passive_vb, passive_vc, r_to_bus)
+
+        # afli
+        line_limits = pd.read_csv(line_limits_file, index_col=0, header=None, squeeze=True).to_dict()
+        self.indexes['afli'] = average_feeding_loading_index(length_to_bus, line_length, line_limits, line_power)
+
 
     def power_balance_index(self, bus_names, load_names, bus_connections, initial_bus):
         """
@@ -454,13 +552,20 @@ class PV:
         else:
             self.selected_phase = None
 
-    def set_power(self, days, month):
+    def set_power(self, days, month, from_array=False, pv_array=None):
         """
         This method creates the solar production based on the month.
         :param days: [str]
         :param month: int
+        :param from_array: bool
+        :param pv_array: np.array
         :return:
         """
+        if from_array:
+            pv_values = pv_array
+        else:
+            pv_values = pv_profile_generator(month)
+
         # initialize the total production series with 0s
         self.P = pd.Series(0, name=days[0] + '-' + days[-1],
                            index=pd.timedelta_range(start='00:00:00', freq='1s', periods=len(days) * 60 * 60 * 24))
@@ -469,7 +574,7 @@ class PV:
         for k, day in enumerate(days):
             start_index = str(k) + ' days'
             end_index = start_index + ' 23:59:59'
-            self.P[start_index:end_index] = self.pv_rated * pv_profile_generator(month)
+            self.P[start_index:end_index] = self.pv_rated * pv_values
 
         self.P *= 1000
 
